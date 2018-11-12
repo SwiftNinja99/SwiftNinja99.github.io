c792cf42b0ae36f4ab0b1b9b333853db7dbfcf8d
mac1 <mac1@133imac14.valleychristian.net>
2018-11-12 09:35:09 -0800

    reeeeeeeeeee

---
+++ scripts/ChrisDay.js
@@ -0,0 +1,20 @@
+var lyrics = [
+            "A Partridge in a Pear Tree",
+            "Two Turtle Doves",
+            "Three French Hens",
+            "Four Calling Birds",
+            "Five Gold Rings",
+            "Six Geese a-Laying",
+            "Seven Swans a-Swimming",
+            "Eight Maids a-Milking",
+            "Nine Ladies Dancing",
+            "Ten Lords a-Leaping",
+            "Eleven Pipers Piping",
+            "Twelve Drummers Drumming"
+        ];
+var chrisDay = prompt("What day of Christmas is it?");
+chrisDay = Number(chrisDay);
+chrisDay--;
+lyrics = lyrics;
+var array1 = [];
+array1 = array1;
